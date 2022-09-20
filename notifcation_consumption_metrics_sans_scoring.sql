740c5c9ed69354fbab2899c0aa050eaff8951af3
Thakur <sthakur@sthakurmbpx1.local>
2022-09-20 15:12:33 -0400

    added in arena and included gametracker events; removed v56

--- notifcation_consumption_metrics_sans_scoring.sql
+++ notifcation_consumption_metrics_sans_scoring.sql
@@ -28,37 +28,14 @@
     FROM `dw_vw.omniture_event_cdm_cbsicbssportssite`
     WHERE
       report_suite_id_nm = 'cbsicbssportssite'
-     AND day_dt BETWEEN '2022-08-01' AND DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
-    --  AND post_evar57_desc LIKE '%top%th%'
+     AND day_dt BETWEEN '2022-01-01' AND DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
     AND post_evar56_desc NOT LIKE '%gametracker%'
---   AND day_dt BETWEEN '2020-05-11' AND '2020-05-17'
-  --    AND LOWER(geo_country_cd) = 'usa'
+    -- AND LOWER(post_evar57_desc) LIKE '%comeback%kings%nfl%judge%'
   )
   , sportsapp AS (
     SELECT * FROM hits
     WHERE post_evar1_desc LIKE '%app%'
     AND post_evar1_desc NOT LIKE '%fantasy_app%'
-  
-  -- CBSSports App Segment
-    -- ((
-    --   post_evar5_desc IN (
-    --   'cbsisqortsapp-android'
-    --   , 'cbsicbssportsapp'
-    --   , 'cbsisportsapp-iphone'
-    --   , 'cbsisportsapp'
-    --   , 'cbsisportsapp-atablet'
-    --   , 'cbsisportsapp-android'
-    --   , 'cbsisportsapp-ipad'
-    --   )
-    --   OR post_evar1_desc IN (
-    --   'cbssports_app_ios'
-    --   , 'cbssports_app_android'
-    --   ))
-    --   AND (
-    --     (page_nm NOT LIKE '%launch screen%' 
-    --       AND page_nm NOT LIKE '%left drawer%')
-    --       OR page_nm IS NULL)
-    --   )
   )
 
   , all_hits as (
@@ -85,22 +62,25 @@ airship_data AS (
 
 SELECT *
 FROM `i-dss-sports-data.sports_vw.airship_sports_push_alert_performance_day`
-WHERE notification_dt BETWEEN '2022-08-01' AND DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+WHERE notification_dt BETWEEN '2021-01-01' AND DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+-- AND LOWER(message_desc) LIKE '%comeback%kings%nfl%judge%'
+-- AND message_desc LIKE '%Padres%'
 )
 
   SELECT * FROM(
+  -- EXCEPT(uniques, visits, page_views, video_plays, video_ad_plays, video_time_spent_mins, video_ad_time_spent_mins), SUM(uniques) AS uniques, SUM(visits) AS visits, SUM(page_views) AS page_views, SUM(video_ad_plays) AS video_ad_plays, SUM(video_time_spent_mins) AS video_time_spent_mins, SUM(video_ad_time_spent_mins) AS video_ad_time_spent_mins  FROM(
   -- Total
   SELECT
-    all_hits.day_dt as ds,
+    notification_dt as ds,
+    -- all_hits.day_dt as ds,
     CASE WHEN post_evar1_desc LIKE '%ios%' THEN 'iOS' WHEN post_evar1_desc LIKE '%android%' THEN 'Android' ELSE 'Other' END AS device_os,
-    post_evar56_desc,
+    COALESCE(REGEXP_EXTRACT(post_evar56_desc, r'^.*?\|(.*?)\|.*?\|.*?\|.*?$'), 'na') as r56_arena, 
     TRIM(REGEXP_REPLACE(post_evar57_desc, r'([^\p{ASCII}]+)', '')) AS post_evar57_desc,
-    REGEXP_CONTAINS(post_evar57_desc, r'([^\p{ASCII}]+)') AS has_emojis,
+    COALESCE(REGEXP_CONTAINS(post_evar57_desc, r'([^\p{ASCII}]+)'),false) AS has_emojis,
     campaign_category_cd,
+    COALESCE(REGEXP_EXTRACT(campaign_category_cd, r'^.*?\,(.*?)$'), 'na') AS cc_arena,
     message_desc,
-    -- 'total' as project,
     user_detail_platform_cd,
-    -- notification_day_nm  notification_time,
     notification_week_of_year_nbr,
     CONCAT(all_hits.day_dt, " ", SAFE.PARSE_TIME('%H:%M',notification_time)) AS notification_timestamp,
     MAX(direct_open_cnt) AS direct_open_cnt,
@@ -108,21 +88,17 @@ WHERE notification_dt BETWEEN '2022-08-01' AND DATE_SUB(CURRENT_DATE(), INTERVAL
     MAX(direct_open_rate_pct) AS direct_open_rate_pct,
     COUNT(DISTINCT post_visitor_id) AS uniques,
     COUNT(DISTINCT visit_session_id) as visits,
-  --   COUNT(DISTINCT visit_session_id) as visits,
     COUNTIF(((page_event_type_id = 0 OR post_page_nm is not null) OR page_event_type_id IN (10,11,12)) AND page_event_type_id = 0) AS page_views,
-    -- COUNTIF(FORMAT("%t", ARRAY(SELECT id FROM UNNEST(post_event_id_list_desc))) LIKE '%718%') AS video_plays2,--media starts
     COUNTIF(FORMAT("%t", ARRAY(SELECT id FROM UNNEST(post_event_id_list_desc))) LIKE '%251%') AS video_plays,
     COUNTIF(FORMAT("%t", ARRAY(SELECT id FROM UNNEST(post_event_id_list_desc))) LIKE '%723%') AS video_ad_plays,
     SUM(COALESCE(SAFE_CAST((SELECT value FROM UNNEST(post_event_id_list_desc) WHERE id = '722') AS INT64),0))/60 AS video_time_spent_mins,
     SUM(COALESCE(SAFE_CAST((SELECT value FROM UNNEST(post_event_id_list_desc) WHERE id = '725') AS INT64),0))/60 AS video_ad_time_spent_mins
-  FROM all_hits
-  JOIN airship_data ad
-  -- ON LOWER(REPLACE(TRIM(REGEXP_REPLACE(message_desc, r'([^\p{ASCII}]+)', '')), '\\n', '')) = LOWER(TRIM(REGEXP_REPLACE(post_evar57_desc, r'([^\p{ASCII}]+)', '')))
+  FROM airship_data ad 
+  LEFT JOIN all_hits
   ON LOWER(REPLACE(TRIM(REGEXP_REPLACE(message_desc, r'[^a-zA-Z]', '')), '\\n', '')) = LOWER(TRIM(REGEXP_REPLACE(post_evar57_desc, r'[^a-zA-Z]', '')))
-
   AND ad.notification_dt = all_hits.day_dt
   AND CASE WHEN LOWER(post_evar1_desc) LIKE '%ios%' THEN 'ios' WHEN LOWER(post_evar1_desc) LIKE '%android%' THEN 'android' ELSE 'other' END = LOWER(user_detail_platform_cd)
-  GROUP BY 1,2,3,4,5,6,7,8,9,10
+  GROUP BY 1,2,3,4,5,6,7,8,9,10,11
   )
-  GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
-  ORDER BY ds DESC, page_views DESC,1,2,3,4,5
+  GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21--,22
+  -- ORDER BY 1 DESC, direct_open_cnt DESC,1,2,3,4,5
